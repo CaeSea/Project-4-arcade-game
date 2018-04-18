434ce424c17e8eab8bf40b41fe4590621ebf232f
Caeron Seabourne <caeking93@gmail.com>
2018-04-18 16:17:10 +0100

    Added player movement functionality

--- js/app.js
+++ js/app.js
@@ -86,6 +86,7 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -94,12 +95,24 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function(keyPressed) {
-  alert(keyPressed);
+  switch (keyPressed) {
+    case 'left':
+        this.x = this.x - 100;
+      break;
+    case 'up':
+        this.y = this.y - 90;
+      break;
+    case 'right':
+        this.x = this.x + 100;
+      break;
+    case 'down':
+        this.y = this.y + 90;
+      break;
+  }
 }
 
 // Place the player object in a variable called player
-let player = new Player();
-
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
